9f451762c9c84b580b00727b9831282e01c8eff3
Ayodele David <dayodele89@outlook.com>
2022-07-30 02:11:41 +0100

    Implemented Cart Fragment UI

---
+++ app/src/main/java/com/example/e_commerceapp/ui/CartFragment.kt
@@ -0,0 +1,48 @@
+package com.example.e_commerceapp.ui
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.e_commerceapp.R
+import com.example.e_commerceapp.databinding.FragmentCartBinding
+import com.example.e_commerceapp.databinding.FragmentHomeBinding
+
+
+class CartFragment : Fragment() {
+lateinit var binding: FragmentCartBinding
+lateinit var mContext: Context
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext=context
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding=FragmentCartBinding.inflate(LayoutInflater.from(mContext),container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/e_commerceapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/e_commerceapp/ui/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.e_commerceapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.e_commerceapp.R
+import com.example.e_commerceapp.adapters.MainViewPagerAdapter
 import com.example.e_commerceapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,13 +16,30 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-supportFragmentManager.beginTransaction().apply {
 
-    replace(R.id.fragment_home,HomeFragment())
-    commit()
+        val mainAdapter=MainViewPagerAdapter(supportFragmentManager,lifecycle)
 
-}
+        binding.vpMain.adapter=mainAdapter
+        binding.vpMain.isUserInputEnabled=false
 
+
+        binding.bottomNavMain.setOnItemSelectedListener(object:NavigationBarView.OnItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId){
+                    R.id.nav_home-> {
+                        binding.vpMain.currentItem=0
+                    return true
+                    }
+                    R.id.nav_cart->{
+                        binding.vpMain.currentItem=1
+                        return true
+                    }
+                }
+            return false
+            }
+
+
+        })
 
 
     }
