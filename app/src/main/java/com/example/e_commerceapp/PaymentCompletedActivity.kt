2aabe7f8c648e4e3f259723b1b44d3755519ee80
Ayodele David <dayodele89@outlook.com>
2022-07-31 00:49:12 +0100

    Implemented Payment and PaymentCompleted Activity UI

---
+++ app/src/main/java/com/example/e_commerceapp/PaymentCompletedActivity.kt
@@ -0,0 +1,11 @@
+package com.example.e_commerceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaymentCompletedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_completed)
+    }
+}
