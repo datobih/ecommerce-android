fd57857376cd047ef9ef4385ea93abdce9f1bac5
Ayodele David <dayodele89@outlook.com>
2022-08-02 09:32:15 +0100

    Implemented WishList Activity UI

---
+++ app/src/main/java/com/example/e_commerceapp/WishListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.e_commerceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WishListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wish_list)
+    }
+}
